e9264dcf553d6fe033e3e1a7caa68d164f7120d5
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-20 21:51:53 -0300

    up

---
+++ Exe006_RafaelMagalhaes.py
@@ -0,0 +1,17 @@
+#Peça ao usuário para inserir um número entre 15 e 20.
+#Se ele inserir um valor abaixo de 15, exiba a mensagem "Muito baixo" e peça que tentem novamente.
+#Se ele inserir um valor acima de 20, exiba a mensagem "Muito alto" e peça que tentem novamente.
+#Continue repetindo isso até que ele insira um valor entre 15 e 20 e, em seguida, exibam a mensagem "Obrigado".
+
+num = int (input('Digite um número entre 15 e 20: '))
+
+while num <= 15 or num >= 20:
+    if num <= 15:
+        print("Muito Baixo! Tente Novamente!")
+        num = int (input('Digite um número entre 15 e 20: '))
+
+    elif num >=20:
+        print("Muito Alto! Tente Novamente")
+        num = int (input('Digite um número entre 15 e 20: '))
+
+print("Obrigado!")
---
+++ Exe07_RafaelMagalhaes.py
@@ -0,0 +1,32 @@
+#Escreva um programa que permaneça em laço lendo números inteiros. 
+#O laço termina quando for digitado 0 (zero).
+#Cada valor diferente de zero digitado deve ser colocado em uma lista, desde que ele ainda não esteja lá, ou seja, valores repetidos não são aceitos. 
+#Se um valor repetido for digitado, o programa deve exibir uma mensagem na tela. 
+#Ao final exiba a lista e a quantidade de elementos que ela contém.
+
+num = []
+
+numdig = int (input("Digite um número: "))
+
+num.append(numdig)
+
+if numdig != 0:
+    while numdig != 0:
+        numdig = int (input("Digite um número: "))
+    
+
+        if numdig in num:
+            print("Valor repetido!")
+
+        else:
+             num.append(numdig)
+
+    if numdig == 0:
+        num.remove(0)
+        print("Lista:",num)
+
+        i = len(num)
+
+        print("A lista tem {} números".format(i))
+            
+    
