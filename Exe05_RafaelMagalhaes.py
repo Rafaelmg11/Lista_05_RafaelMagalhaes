a3f2d089ce43fe8a57f2a0902064fcb16d7a0542
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-20 21:20:48 -0300

    up

--- Exe05_RafaelMagalhaes.py
+++ Exe05_RafaelMagalhaes.py
@@ -1,7 +1,26 @@
 #Crie uma variavel chamada "adivinha" e defina o valor como 50.
-#Peça ao usuario para inserie um número. Embora o palpite não seja o mesmo que o valor do "adivinha", diga a ele se o palpite é  muito alto ou muito baixo e peça que ele dê outro
+#Peça ao usuario para inserir um número. Embora o palpite não seja o mesmo que o valor do "adivinha", diga a ele se o palpite é  muito alto ou muito baixo e peça que ele dê outro
 #palpite. Se ele inserir o mesmo valor que "adivinha",exiba a mensagem "Parabéns! Você acertou o número em {} tentativas!"
 
-adivinha = 47
+adivinha = 50
+i = 1
 
-num = int (input("Insira um número: "))
+palpite = int (input("Insira o seu palpite: "))
+
+if palpite != adivinha:
+    while palpite != adivinha:
+        if palpite > adivinha:
+            print("Palpite muito alto!")
+        elif palpite < adivinha:
+            print("Palpite muito baixo!") 
+
+        palpite = int (input("Insira o seu palpite: "))
+
+        
+
+        i+=1
+
+    print("Parabéns! Você acertou o número em {} tentativas!".format(i))
+
+elif palpite == adivinha:
+    print("Parabéns você acertou de primeira! ")
